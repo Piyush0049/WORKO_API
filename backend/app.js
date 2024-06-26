a86778f196d3e4608636dbde72579ce036001b38
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-06-26 16:29:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import { config } from "dotenv";
 
@@ -10,7 +9,6 @@ config({
 });
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
 
 app.get("/", (req, res) => {
